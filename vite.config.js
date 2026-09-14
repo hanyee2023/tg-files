26bc36868b970e619d9168f27c8909907c354a99
hanyee2023 <142211294+hanyee2023@users.noreply.github.com>
2026-09-14 11:28:09 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+  define: {
+    global: 'globalThis',
+  },
+});
